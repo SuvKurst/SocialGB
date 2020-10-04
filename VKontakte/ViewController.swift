e040896ec79db933c45f79b9c7db2392670a60ad
Kir <suvkurst@mail.ru>
2020-10-04 15:10:37 +0300

    1lesson

---
+++ VKontakte/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  VKontakte
+//
+//  Created by компьютер  on 27.09.2020.
+//  Copyright © 2020 компьютер . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
